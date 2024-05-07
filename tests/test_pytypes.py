d0d3258cba6f7f091c34ab7bf33dde607c3ea526
Adam Chidlow <achidlow@users.noreply.github.com>
2024-05-07 10:11:53 +0800

    first crack at testing pytypes for stubs

--- tests/test_pytypes.py
+++ tests/test_pytypes.py
@@ -1,28 +1,56 @@
-import re
-
 import pytest
-
 from puya.awst_build import pytypes
+
 from tests import VCS_ROOT
 
 _STUB_SUFFIX = ".pyi"
 
 
 def stub_class_names_and_predefined_aliases() -> list[str]:
-    stubs_dir = VCS_ROOT / "stubs" / "algopy-stubs"
-    result = []
+    from mypy import build, find_sources, fscache, nodes
+    from puya.compile import get_mypy_options
 
-    for pyi_path in stubs_dir.rglob(f"*{_STUB_SUFFIX}"):
-        assert pyi_path.is_file()
-        rel_path = pyi_path.relative_to(stubs_dir).with_suffix("")
-        module_name = "algopy." + ".".join(rel_path.parts).removesuffix(".__init__")
-        with pyi_path.open() as fp:
-            for ln in fp:
-                m = re.match(r"class ([^_][^:(]+)", ln)
-                if m:
-                    (class_name,) = m.groups()
-                    result.append(".".join((module_name, class_name)))
-    return result
+    stubs_dir = (VCS_ROOT / "stubs" / "algopy-stubs").resolve()
+    mypy_options = get_mypy_options()
+    fs_cache = fscache.FileSystemCache()
+    mypy_build_sources = find_sources.create_source_list(
+        paths=[str(stubs_dir)], options=mypy_options, fscache=fs_cache
+    )
+    build_result = build.build(sources=mypy_build_sources, options=mypy_options, fscache=fs_cache)
+    result = set()
+
+    algopy_module = build_result.files["algopy"]
+    modules_to_visit = [algopy_module]
+    seen_modules = set()
+    while modules_to_visit:
+        module = modules_to_visit.pop()
+        if module in seen_modules:
+            continue
+        seen_modules.add(module)
+        for name, symbol in module.names.items():
+            if name.startswith("_") or symbol.module_hidden or symbol.kind != nodes.GDEF:
+                continue
+            match symbol.node:
+                case nodes.MypyFile() as new_module:
+                    modules_to_visit.append(new_module)
+                case nodes.TypeAlias(fullname=alias_name):
+                    result.add(alias_name)
+                case nodes.TypeInfo(fullname=class_name):
+                    result.add(class_name)
+                case _:
+                    pass
+
+    # for pyi_path in stubs_dir.rglob(f"*{_STUB_SUFFIX}"):
+    #     assert pyi_path.is_file()
+    #     rel_path = pyi_path.relative_to(stubs_dir).with_suffix("")
+    #     module_name = "algopy." + ".".join(rel_path.parts).removesuffix(".__init__")
+    #     with pyi_path.open() as fp:
+    #         for ln in fp:
+    #             m = re.match(r"class ([^_][^:(]+)", ln)
+    #             if m:
+    #                 (class_name,) = m.groups()
+    #                 result.add(".".join((module_name, class_name)))
+    return sorted(result)
 
 
 @pytest.mark.parametrize(
